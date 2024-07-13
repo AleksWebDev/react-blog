63cd9e1d6b555b2740fc85fa20b0b28d84f26acf
AleksWebDev <lavrovweb@gmail.com>
2024-07-14 01:45:38 +0300

    deleting post

--- src/BlogDetailse.js
+++ src/BlogDetailse.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 function BlogDetails() {
 
     const {id} = useParams()
     const {data: blog, isLoaded, error} = useFetch('http://localhost:8000/posts/' + id);
+    const navigate = useNavigate();
+
+
+    const deletePost = () => {
+        fetch('http://localhost:8000/posts/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            navigate('/')
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -14,6 +25,7 @@ function BlogDetails() {
                     <h2>{blog.title}</h2>
                     <p className="author">Written by: {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={deletePost} className="btn-delete">Delete</button>
                 </article>
             )}
         </div>
--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,39 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Create() {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Mary Jane')
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsPending(true);
+        const blog = {title, body, author};
+        
+        setTimeout(() => {
+            fetch('http://localhost:8000/posts', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(blog),
+        }).then(() => {
+            setIsPending(false);
+            setAuthor('Mary Jane');
+            setBody('');
+            setTitle('');
+            navigate('/')
+        })
+        }, 500)
+    }
 
     return ( 
         <div className="create">
             <h2>Add new post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Post title</label>
                 <input type="text" required value={title} onChange={(e) => {setTitle(e.target.value)}}/>
 
@@ -22,6 +46,9 @@ function Create() {
                     <option value="Mary Jane">Mary Jane</option>
                     <option value="Tom Soyer">Tom Soyer</option>
                 </select>
+
+                {isPending && <button disabled>Adding post...</button>}
+                {!isPending && <button>Create Post</button>}
             </form>
         </div>
     );
